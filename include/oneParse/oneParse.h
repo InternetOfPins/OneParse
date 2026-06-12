79538a6669a1ae2c86e6f022e8df7bc0516a666b
neu-rah <ruihfazevedo@gmail.com>
2026-06-12 17:56:54 +0000

    initial OneParse scaffold — HAPI parser combinator components

---
+++ include/oneParse/oneParse.h
@@ -0,0 +1,14 @@
+/**
+ * @file oneParse.h
+ * @author Rui Azevedo (neu-rah) (ruihfazevedo@gmail.com)
+ * @brief OneParse - Parser combinator components for HAPI and embedded systems
+ */
+
+#pragma once
+
+#include <hapi/hapi.h>
+using hapi::APIOf;
+
+namespace oneParse {
+
+}; // namespace oneParse
